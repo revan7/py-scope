778543271eea10ce408613212f2ecdee028dc695
revan7 <tlmatei@yahoo.com>
2020-08-27 18:36:57 +0200

    Add picture taking ability

--- camera.py
+++ camera.py
@@ -3,13 +3,15 @@
 # http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 
 import io
-import picamera
 import logging
 import socketserver
-from threading import Condition
+from datetime import datetime
 from http import server
+from threading import Condition
 
-PAGE="""\
+import picamera
+
+PAGE = """\
 <html>
 <head>
 <title>Raspberry Pi - Surveillance Camera</title>
@@ -20,6 +22,7 @@ PAGE="""\
 </body>
 </html>
 """
+
 
 class StreamingOutput(object):
     def __init__(self):
@@ -38,6 +41,7 @@ class StreamingOutput(object):
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
+
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
@@ -51,6 +55,15 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
+        elif self.path == '/snap.html':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            pic_camera = picamera.PiCamera()
+            n = datetime.now()
+            t = n.timetuple()
+            c = str(t[3]) + str(t[4])
+            pic_camera.capture('/home/pi/Documents/scope/image%s.jpg' % c)
+            self.end_headers()
         elif self.path == '/stream.mjpg':
             self.send_response(200)
             self.send_header('Age', 0)
@@ -77,14 +90,16 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_error(404)
             self.end_headers()
 
+
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
+
 with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
-    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    #camera.rotation = 90
+    # Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    # camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
     try:
         address = ('', 8000)
